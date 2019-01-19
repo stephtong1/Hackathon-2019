ed6bb388cae3794022a5b5dd4ecad140802abda3
Tommy Trinh <tommy.trinh@polymtl.ca>
2019-01-19 14:20:16 -0500

    adding files

--- NidDePoule/App.js
+++ NidDePoule/App.js
@@ -1,62 +1,47 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import { StyleSheet, Button, View } from 'react-native';
 
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
 
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    }
+  constructor(props){
+    super(props);
+    this.state = {currentColor: "#841584"};
   }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
+  setRandomColor = () => {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
 
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
+    this.setState( () => {
+      return { currentColor: color };
+    });
 
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
+    console.log('button clicked!')
+    console.log(color);
+    console.log(this.state);
   };
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={this.setRandomColor}
+          title="Change color"
+          color={this.state.currentColor}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });

9deca7aaf33099359641e7ca78213d634d9f5398
Teepaca <stephanie.tong@polymtl.ca>
2019-01-19 19:42:31 -0500

    added live user location, and prototype marker

--- NidDePoule/App.js
+++ NidDePoule/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Platform, AppRegistry, Button } from 'react-native';
-import { Constants, Location, Permissions } from 'expo';
-import { MapView } from 'expo';
+import { Constants, Location, Permissions, Google } from 'expo';
+import MapView from 'react-native-maps';
 
 
 export default class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {currentColor: "#841584"};
+    this.state = {
+      currentColor: "#841584",
+      location: {
+        coords: {
+          latitude: 46.794219,
+          longitude: -71.244461
+        }
+      }
+    };
+    this._getLocationAsync();
+    console.log("#######" + this.state.location.coords.latitude + "#########")
   }
 
   componentWillMount() {
-    if (Platform.OS === 'android' && !Constants.isDevice) {
-      this.setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-      });
-    } else {
-      this._getLocationAsync();
-    }
+    this._getLocationAsync();
   }
 
   componentDidMount() {
@@ -33,9 +37,12 @@ export default class App extends React.Component {
       });
     }
 
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location });
-  };
+    let locationFetch = await Location.getCurrentPositionAsync({});
+    this.setState(() => {
+      return { location: locationFetch };
+    });
+
+  }
 
   setRandomColor = () => {
     var letters = '0123456789ABCDEF';
@@ -44,7 +51,7 @@ export default class App extends React.Component {
       color += letters[Math.floor(Math.random() * 16)];
     }
 
-    this.setState( () => {
+    this.setState(() => {
       return { currentColor: color };
     });
 
@@ -52,23 +59,66 @@ export default class App extends React.Component {
     console.log(color);
     console.log(this.state);
   };
-  
+
+  pinMarker(latitude, longitude) {
+    return (
+      <MapView.Marker
+        coordinate={
+          {
+            latitude: latitude,
+            longitude: longitude
+          }
+        }
+        title="This is a title"
+        description="This is a description"
+      />)
+  }
+
   render() {
     let text = 'Waiting..';
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.location) {
+    let longlong = 0;
+    let latlat = 0;
+
+    if (this.state.location) {
       text = JSON.stringify({
-        longitude : this.state.location.coords.longitude, latitude : + this.state.location.coords.latitude},
+        latitude: this.state.location.coords.latitude,
+        longitude: this.state.location.coords.longitude
+      },
         null, '\t');
+      latlat = this.state.location.coords.latitude;
+      longlong = this.state.location.coords.longitude;
+
+      console.log("@@@@@@@@@@" + longlong + "@@@@@@@" + latlat + "@@@@@@@")
     }
+
     return (
       <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
+        <Text style={styles.paragraph}>{text}</Text>
+        <MapView
+          style={{ flex: 1 }}
+          ref={(mapView) => { _mapView = mapView; }}
+          provider="google"
+          initialRegion={{
+            latitude: latlat,
+            longitude: longlong,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          onUserLocationChange={() => {
+            this._getLocationAsync(6);
+            _mapView.animateToCoordinate({
+              latitude: this.state.location.coords.latitude,
+              longitude: this.state.location.coords.longitude
+            }, 1000)
+          }}
+          showsUserLocation
+        >
+        {this.pinMarker(this.state.location.coords.latitude, this.state.location.coords.longitude)}
+        </MapView>
         <Button
           onPress={() => {
             this.setRandomColor();
-            this._getLocationAsync();
+            this._getLocationAsync(6);
           }}
           title="Change color"
           color={this.state.currentColor}
@@ -81,9 +131,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'relative'
   },
   paragraph: {
     margin: 24,
